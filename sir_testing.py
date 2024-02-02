50d67f1bea50e4a2744b321d4dcea6c970ada187
Timur Kasimov <104742862+timurka43@users.noreply.github.com>
2024-02-02 09:51:14 -0600

    updated sir_testing.py

--- sir_testing.py
+++ sir_testing.py
@@ -9,12 +9,23 @@ Need to define the two functions you will feed to your newton method
 '''
 
 
-def dIdt(t):
+def dIdt(t):  
     ##need to calculate S(t) and I(t) first
-    
+    St, It, Rt = sir(init, t, R0)
     ## then use formula for dIdt: R0SI - I
-    R0*
-    return
+    return R0*St*It - It
+'''
+    Current issues in the function:
+    need to define init, R0, since these are
+    not imported from sir.py since they're under 
+    __main__ block.
+
+    You can define them globally 
+    or, best,
+    rewrite your Newton method in such a way so that 
+    you can specifiy init and R0 everytime you call the function.
+'''
+    
 
 def dI2dt2(t):
     return
